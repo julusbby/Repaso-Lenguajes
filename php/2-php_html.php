84f567e3690440a9d8a4e3d72ea95cd1272773e8
juliana <julusbby530@gmal.com>
2026-07-29 12:09:15 -0500

    cambios2

---
+++ php/2-php_html.php
@@ -0,0 +1,15 @@
+<h1>Codigo HTML</h1>
+
+<?php
+    echo "<h1>Hola mundo desde php</h1>";
+    print("Hola");
+
+    //comentario de una sola linea
+
+    echo "<!--Comentario HTML-->";
+
+    #Variable
+    $texto = "Esto es un texto";
+
+    echo "<h1>$texto</h1>";
+?>
